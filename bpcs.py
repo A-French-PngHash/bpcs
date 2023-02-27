c00e21f90b01ccdec1af3996e7cbad93a29f3665
Titouan <75045140+PngHash-Titouan@users.noreply.github.com>
2023-02-27 22:16:45 +0100

    remove useless file update readme

--- bpcs.py
+++ bpcs.py
@@ -1,35 +1,13 @@
-import getopt
-import sys
 """
-bpcs
+Example
 
-Command line options.
-
-    -v "vessel image": provides the path to the host image.
-    -h "hidden data file": provides the path to the secret data.
-    -c {complexity}: Set the desired complexity threshold, must be strictly inferior to 0.5.
-
-    -a: Analyses the storage capacity/needed (if vessel/if hidden) of the file provided.
+secret = SecretData(data_loader.load_file_as_binary('TestImage/girl128bw.png'), complexityThreshold=0.3)
+host = HostImageColor(host_path='TestImage/girl512color.png', complexity_threshold=0.3)
+encoder = Encoder(host_image=host, complexity_threshold=0.3, secret_data=secret, file_name='a.png')
+new_host = encoder.encode()
+new_host.write_image_to("TestImage/hidden/test.png")
 
 
+decoder = Decoder('TestImage/hidden/test.png', complexity_threshold=0.3, black_white=False)
+decoder.decode()
 """
-
-def main():
-    opts = getopt.getopt(sys.argv[1:], "v:h:c:a:", [])
-    complexity = None
-    host_path = None
-    hidden_path = None
-    analyse = False
-
-    for opt in opts:
-        if opt[0] == "-c":
-            complexity = opt[1]
-        elif opt[0] == "-v":
-            host_path = opt[1]
-        elif opt[0] == "-h":
-            hidden_path = opt[1]
-
-
-
-if __name__ == "__main__":
-    main()
--- readme.md
+++ readme.md
@@ -1,2 +1,12 @@
-## Encoding
-The encoding is done starting by the top most CGC layer, by the top left block. Each complex block will be encoded with a complex secret block. For now, the end will be marked with white check block followed by black check block.
+# Introduction
+Implementation of the Bit Plane Complexity Segmentation algorithm to hide data in images by Eiji Kawaguchi and Richard O.
+The algorithm replaces the complex regions of the image (that the eye cannot make a sense of anyway) with the hidden data. Enabling a **storage capacity of 60-40%** of the original image size, which is significantly higher than other steganography techniques such as LSB (Least Significant Bit).
+
+## Coding style
+The code is fully object oriented, here is a description of the main classes :
+
+- **HostImage(BW/Color)** : A vessel image, in black and white or in color.
+- **BitPlane64(ConjugateBit)** : An 8*8 block of pixel.
+- **SecretData** : The data to hide.
+- **Encoder** : His goal is to hide the data into the vessel.
+- **Decoder** : His goal is to retrieve the data from the vessel.
